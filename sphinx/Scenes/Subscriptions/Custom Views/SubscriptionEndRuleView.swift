aa1f84da35c729d893929f8922ada59f1157043b
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-09-10 10:30:19 -0300

    Fix for preferredDatePickerStyle only available from iOS 13.4

--- sphinx/Scenes/Subscriptions/Custom Views/SubscriptionEndRuleView.swift
+++ sphinx/Scenes/Subscriptions/Custom Views/SubscriptionEndRuleView.swift
@@ -123,7 +123,9 @@ class SubscriptionEndRuleView : SubscriptionCommonView {
         datePicker.minimumDate = Date()
         datePicker.timeZone = TimeZone(abbreviation: "UTC")
         datePicker.date = selectedDate
-        datePicker.preferredDatePickerStyle = .wheels
+        if #available(iOS 13.4, *) {
+            datePicker.preferredDatePickerStyle = .wheels
+        }
         datePicker.addTarget(self, action: #selector(datePickerChanged(picker:)), for: .valueChanged)
         return datePicker
     }

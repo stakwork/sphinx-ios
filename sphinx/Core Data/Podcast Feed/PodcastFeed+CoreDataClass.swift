c8e51e599f9d795ee63915416e0704c802c20437
Tomas Timinskas <tomasalbertotiminskas@gmail.com>
2021-09-30 10:26:52 -0300

    Fix for subscribed podcast using podcastId instead chatId (null) to store current state info

--- sphinx/Core Data/Podcast Feed/PodcastFeed+CoreDataClass.swift
+++ sphinx/Core Data/Podcast Feed/PodcastFeed+CoreDataClass.swift
@@ -11,49 +11,55 @@ import CoreData
 @objc(PodcastFeed)
 public class PodcastFeed: NSManagedObject {
     
+    var identifier: Int {
+        get {
+            chat?.id ?? Int(id)
+        }
+    }
+    
     var currentEpisode: Int {
         get {
-            return (UserDefaults.standard.value(forKey: "current-episode-\(chat?.id ?? -1)") as? Int) ?? 0
+            return (UserDefaults.standard.value(forKey: "current-episode-\(identifier)") as? Int) ?? 0
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "current-episode-\(chat?.id ?? -1)")
+            UserDefaults.standard.set(newValue, forKey: "current-episode-\(identifier)")
         }
     }
     
     var currentEpisodeId: Int {
         get {
-            return (UserDefaults.standard.value(forKey: "current-episode-id-\(chat?.id ?? -1)") as? Int) ?? -1
+            return (UserDefaults.standard.value(forKey: "current-episode-id-\(identifier)") as? Int) ?? -1
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "current-episode-id-\(chat?.id ?? -1)")
+            UserDefaults.standard.set(newValue, forKey: "current-episode-id-\(identifier)")
         }
     }
     
     var lastEpisodeId: Int? {
         get {
-            return (UserDefaults.standard.value(forKey: "last-episode-id-\(chat?.id ?? -1)") as? Int)
+            return (UserDefaults.standard.value(forKey: "last-episode-id-\(identifier)") as? Int)
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "last-episode-id-\(chat?.id ?? -1)")
+            UserDefaults.standard.set(newValue, forKey: "last-episode-id-\(identifier)")
         }
     }
     
     var currentTime: Int {
         get {
-            return (UserDefaults.standard.value(forKey: "current-time-\(chat?.id ?? -1)") as? Int) ?? 0
+            return (UserDefaults.standard.value(forKey: "current-time-\(identifier)") as? Int) ?? 0
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "current-time-\(chat?.id ?? -1)")
+            UserDefaults.standard.set(newValue, forKey: "current-time-\(identifier)")
         }
     }
     
     var playerSpeed: Float {
         get {
-            let speed = (UserDefaults.standard.value(forKey: "player-speed-\(chat?.id ?? -1)") as? Float) ?? 1.0
+            let speed = (UserDefaults.standard.value(forKey: "player-speed-\(identifier)") as? Float) ?? 1.0
             return speed >= 0.5 && speed <= 2.1 ? speed : 1.0
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "player-speed-\(chat?.id ?? -1)")
+            UserDefaults.standard.set(newValue, forKey: "player-speed-\(identifier)")
         }
     }
     

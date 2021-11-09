db530c6da6d0a136a9637cc8bfb6d65bc625a83a
CypherPoet <CypherPoet@gmail.com>
2021-11-09 04:14:54 -0600

    Functionality and UI for handling of video player item loading states.

--- sphinx/Scenes/Video Feed/Video Feed Episode Player/GeneralVideoFeedEpisodePlayerViewController GeneralVideoFeedEpisodePlayerViewController.swift
+++ sphinx/Scenes/Video Feed/Video Feed Episode Player/GeneralVideoFeedEpisodePlayerViewController GeneralVideoFeedEpisodePlayerViewController.swift
@@ -17,8 +17,20 @@ class GeneralVideoFeedEpisodePlayerViewController: UIViewController, VideoFeedEp
     
     
     private lazy var avPlayerViewController = makeAVPlayerViewController()
-    private var avPlayerItem: AVPlayerItem?
-    private var avPlayer: AVPlayer?
+    private lazy var loadingViewController = LoadingViewController()
+    
+    private var avPlayerAsset: AVAsset!
+    private var avPlayerItem: AVPlayerItem!
+    private var avPlayer: AVPlayer!
+    
+    
+    private let requiredAssetKeys = [
+        "playable",
+        "hasProtectedContent"
+    ]
+    
+    // Key-value observing context
+    private var playerItemContext = 0
     
     
     var videoPlayerEpisode: Video! {
@@ -45,7 +57,7 @@ extension GeneralVideoFeedEpisodePlayerViewController {
             .instantiate()
         
         viewController.videoPlayerEpisode = videoPlayerEpisode
-    
+        
         return viewController
     }
 }
@@ -56,9 +68,8 @@ extension GeneralVideoFeedEpisodePlayerViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupViews()
-        setupAVPlayerViewController()
     }
     
     
@@ -95,7 +106,7 @@ extension GeneralVideoFeedEpisodePlayerViewController {
     
     private func makeAVPlayerViewController() -> AVPlayerViewController {
         let playerViewController = AVPlayerViewController()
-
+        
         playerViewController.delegate = self
         playerViewController.showsPlaybackControls = true
         playerViewController.entersFullScreenWhenPlaybackBegins = false
@@ -103,20 +114,77 @@ extension GeneralVideoFeedEpisodePlayerViewController {
         
         return playerViewController
     }
-        
     
-    private func setupAVPlayerViewController() {
+    
+    func addAVPlayerViewController() {
         addChildVC(
             child: avPlayerViewController,
             container: videoPlayerView
         )
     }
     
+    private func removeAVPlayerViewController() {
+        removeChildVC(child: avPlayerViewController)
+    }
+
+    
+    
+    private func addPlayerErrorView() {
+        // 📝 TODO:  Design / define how errors here should be presented to users
+//        addChildVC(
+//            child: playerErrorViewController,
+//            container: videoPlayerView
+//        )
+    }
+    
+    private func removePlayerErrorView() {
+        // 📝 TODO:  Design / define how errors here should be presented to users
+//        removeChildVC(child: playerErrorViewController)
+    }
+    
+    
+    private func addPlayerLoadingView() {
+        addChildVC(
+            child: loadingViewController,
+            container: videoPlayerView
+        )
+    }
+
+    
+    private func removePlayerLoadingView() {
+        removeChildVC(child: loadingViewController)
+    }
+    
+    
     
     private func updateVideoPlayer(withNewEpisode video: Video) {
         guard let mediaURL = video.mediaURL else { return }
-  
-        avPlayerItem = .init(asset: AVURLAsset(url: mediaURL))
+        
+        removeAVPlayerViewController()
+        removePlayerErrorView()
+        addPlayerLoadingView()
+
+        setupPlayerAsset(usingURL: mediaURL)
+
+        episodeTitleLabel.text = videoPlayerEpisode.titleForDisplay
+        episodeViewCountLabel.text = "\(Int.random(in: 100...999)) Views"
+        episodePublishDateLabel.text = videoPlayerEpisode.publishDateText
+    }
+    
+    
+    private func setupPlayerAsset(usingURL mediaURL: URL) {
+        avPlayerAsset = AVAsset(url: mediaURL)
+        avPlayerItem = AVPlayerItem(asset: avPlayerAsset)
+        
+        
+        // Register as an observer of the player item's status property
+        avPlayerItem.addObserver(
+            self,
+            forKeyPath: #keyPath(AVPlayerItem.status),
+            options: [.old, .new],
+            context: &playerItemContext
+        )
+        
         
         if avPlayer == nil {
             avPlayer = AVPlayer(playerItem: avPlayerItem)
@@ -124,14 +192,68 @@ extension GeneralVideoFeedEpisodePlayerViewController {
         } else {
             avPlayer!.replaceCurrentItem(with: avPlayerItem!)
         }
-        
-        episodeTitleLabel.text = videoPlayerEpisode.titleForDisplay
-        episodeViewCountLabel.text = "\(Int.random(in: 100...999)) Views"
-        episodePublishDateLabel.text = videoPlayerEpisode.publishDateText
     }
 }
 
 
+// MARK: -  AVPlayerItem status observation
+extension GeneralVideoFeedEpisodePlayerViewController {
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        // Only handle observations for the playerItemContext
+        guard context == &playerItemContext else {
+            super.observeValue(
+                forKeyPath: keyPath,
+                of: object,
+                change: change,
+                context: context
+            )
+            
+            return
+        }
+        
+        
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+            
+            // Switch over status value
+            switch status {
+            case .readyToPlay:
+                // Player item is ready to play.
+                removePlayerLoadingView()
+                removePlayerErrorView()
+                
+                addAVPlayerViewController()
+            case .failed:
+                // Player item failed. See error.
+                removeAVPlayerViewController()
+                removePlayerLoadingView()
+                
+                addPlayerErrorView()
+            case .unknown:
+                // Player item is not yet ready.
+                removeAVPlayerViewController()
+                removePlayerErrorView()
+                
+                addPlayerLoadingView()
+            }
+        }
+    }
+}
+
+
+// MARK: -  AVPlayerViewControllerDelegate
 extension GeneralVideoFeedEpisodePlayerViewController: AVPlayerViewControllerDelegate {
     
     func playerViewController(

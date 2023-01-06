29c4ae155e022373ef5d0d177986b38d53b660cd
Jim Carucci <jim.carucci@dibbstechnology.com>
2023-01-06 09:44:49 -0600

    do content feed fetch before caching

--- sphinx/Scenes/Dashboard/Feeds/All Feeds/AllTribesManager.swift
+++ sphinx/Scenes/Dashboard/Feeds/All Feeds/AllTribesManager.swift
@@ -21,6 +21,8 @@ class AllTribesManager : NSObject{
         if UserDefaults.Keys.shouldAutoDownloadSubscribedPods.get(defaultValue: false) == false{
             return
         }
+        
+
         //1. Fetch results from memory
         guard
             let resultController = fetchedResultsController as? NSFetchedResultsController<NSManagedObject>,
@@ -39,23 +41,34 @@ class AllTribesManager : NSObject{
         print(followedPodFeeds)
         //2. Walk through each podcast feed
         for feed in followedPodFeeds{
-            let _ = Array(feed.items ?? []).map({
-                print("ItemID:\($0.itemID) ObjectID:\($0.objectID) Title:\($0.title) Date:\($0.dateUpdated)")
-                print("-----")
-            })
-            //2. Pluck the latest episode and download it for each feed
-            let lastItem = Array(feed.items ?? []).sorted { (first, second) in
-                let firstDate = first.dateUpdated ?? first.datePublished ?? Date.init(timeIntervalSince1970: 0)
-                let secondDate = second.dateUpdated ?? second.datePublished ?? Date.init(timeIntervalSince1970: 0)
-
-                return firstDate > secondDate
-            }.first
-            if let valid_item = lastItem{
-                var lastEpisode = PodcastEpisode.convertFrom(contentFeedItem: valid_item)
-                //print(lastEpisode.title)
-                let downloadService = DownloadService.sharedInstance
-                downloadService.startDownload(lastEpisode)
+            if let valid_url = feed.feedURL{
+                ContentFeed.fetchFeedItemsInBackground(feedUrl: valid_url.absoluteString, contentFeedObjectID: feed.objectID, completion: {
+                    self.downloadLastPod(feed: feed)
+                })
             }
+            else{
+                downloadLastPod(feed: feed)
+            }
+        }
+    }
+    
+    func downloadLastPod(feed:ContentFeed){
+        let _ = Array(feed.items ?? []).map({
+            print("ItemID:\($0.itemID) ObjectID:\($0.objectID) Title:\($0.title) Date:\($0.dateUpdated)")
+            print("-----")
+        })
+        //2. Pluck the latest episode and download it for each feed
+        let lastItem = Array(feed.items ?? []).sorted { (first, second) in
+            let firstDate = first.dateUpdated ?? first.datePublished ?? Date.init(timeIntervalSince1970: 0)
+            let secondDate = second.dateUpdated ?? second.datePublished ?? Date.init(timeIntervalSince1970: 0)
+
+            return firstDate > secondDate
+        }.first
+        if let valid_item = lastItem{
+            var lastEpisode = PodcastEpisode.convertFrom(contentFeedItem: valid_item)
+            //print(lastEpisode.title)
+            let downloadService = DownloadService.sharedInstance
+            downloadService.startDownload(lastEpisode)
         }
     }
     

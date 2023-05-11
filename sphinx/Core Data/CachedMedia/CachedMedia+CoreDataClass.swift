2862c6261ad66b25e0449b87994d952049db37b5
Jim Carucci <jim.carucci@dibbstechnology.com>
2023-05-11 15:26:11 -0500

    Add CachedMedia CoreData class

---
+++ sphinx/Core Data/CachedMedia/CachedMedia+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CachedMedia+CoreDataClass.swift
+//  sphinx
+//
+//  Created by James Carucci on 5/11/23.
+//  Copyright © 2023 sphinx. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import SwiftyJSON
+
+@objc(CachedMedia)
+public class CachedMedia: NSManagedObject {
+    
+    
+}

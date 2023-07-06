486f8af7ff65c508ce9144fe1361528a9f06fcc9
Jim Carucci <96802642+uncleJim21@users.noreply.github.com>
2023-07-06 14:14:29 -0500

    adapt to dark mode

--- sphinx/Scenes/Profile/Table View Cells/MaxContentAgeTableViewCell.swift
+++ sphinx/Scenes/Profile/Table View Cells/MaxContentAgeTableViewCell.swift
@@ -18,6 +18,7 @@ class MaxContentAgeTableViewCell: UITableViewCell {
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var customAgeTextField: UITextField!
     @IBOutlet weak var daysLabel: UILabel!
+    @IBOutlet weak var customAgeContainerView: UIView!
     
     var delegate : MaxContentAgeTableViewCellDelegate? = nil
     static let reuseID = "MaxContentAgeTableViewCell"
@@ -42,7 +43,7 @@ class MaxContentAgeTableViewCell: UITableViewCell {
     override func prepareForReuse() {
         self.isSelectedRow = false
         durationLabel.isHidden = false
-        customAgeTextField.isHidden = true
+        customAgeContainerView.isHidden = true
         daysLabel.isHidden = customAgeTextField.isHidden
     }
     
@@ -50,17 +51,37 @@ class MaxContentAgeTableViewCell: UITableViewCell {
         switch(age){
         case .customDays:
             durationLabel.isHidden = true
-            customAgeTextField.isHidden = false
+            customAgeContainerView.isHidden = false
             customAgeTextField.text = String(UserData.sharedInstance.getCustomMaxAgeValueInDays())
             break
         default:
             durationLabel.isHidden = false
             durationLabel.text = age.localizedDescription
-            customAgeTextField.isHidden = true
+            customAgeContainerView.isHidden = true
             break
         }
-        daysLabel.isHidden = customAgeTextField.isHidden
+        daysLabel.isHidden = customAgeContainerView.isHidden
+        setupTextField()
+    }
+    
+    func setupTextField(){
         customAgeTextField.delegate = self
+        customAgeTextField.font = UIFont(name: "Roboto", size: 17.0)
+        customAgeContainerView.layer.cornerRadius = customAgeTextField.frame.height/2.0
+        
+          if #available(iOS 13.0, *) {
+              if UIScreen.main.traitCollection.userInterfaceStyle == .dark {
+                  // Dark mode is active
+                  print("Dark mode")
+                  customAgeContainerView.backgroundColor = UIColor(hex: "#131B1D")
+              } else {
+                  // Light mode is active
+                  print("Light mode")
+                  customAgeContainerView.backgroundColor = UIColor(hex: "#CECECE")
+              }
+          } else {
+              customAgeContainerView.backgroundColor = UIColor(hex: "#CECECE")
+          }
     }
 }
 

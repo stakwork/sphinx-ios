05977769269e2e2dc33319ca19314e331fe363e8
Korede612 <korede.oko-osi@nytengaletech.com>
2024-03-03 18:21:32 +0100

    feat: Tapping Feed tab shows Feed Content

--- sphinxUITests/DashboardUITest.swift
+++ sphinxUITests/DashboardUITest.swift
@@ -10,25 +10,28 @@ import XCTest
 
 final class DashboardUITest: XCTestCase {
 
+//    var app: XCUIApplication!
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
+        app.launch()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testTappingFeedsTabShowFeedContent() throws {
+        let feedTab = app/*@START_MENU_TOKEN@*/.staticTexts["Feed"]/*[[".buttons[\"Feed\"].staticTexts[\"Feed\"]",".staticTexts[\"Feed\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        XCTAssertTrue(feedTab.exists)
+        feedTab.tap()
+        let dashboardFeedsContainerVC = app.otherElements["DashboardFeedsContainerViewController"]
+        XCTAssertTrue(dashboardFeedsContainerVC.exists)
     }
 
     func testLaunchPerformance() throws {

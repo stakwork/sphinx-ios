d5dc9d9b2516ae82570cf3757efe529bb7f2e9eb
Korede612 <korede.oko-osi@nytengaletech.com>
2024-03-03 18:27:15 +0100

    feat: Tapping Add Tribes button present Discover Tribe View

--- sphinxUITests/DashboardUITest.swift
+++ sphinxUITests/DashboardUITest.swift
@@ -14,12 +14,8 @@ final class DashboardUITest: XCTestCase {
     let app = XCUIApplication()
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
         app.launch()
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
@@ -49,6 +45,13 @@ final class DashboardUITest: XCTestCase {
         XCTAssertTrue(chatViewController.exists)
     }
     
+    func testTappingAddTribesShowDiscoverTribesView() {
+        app/*@START_MENU_TOKEN@*/.staticTexts["Tribes"]/*[[".buttons[\"Tribes\"].staticTexts[\"Tribes\"]",".staticTexts[\"Tribes\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["Add Tribe"]/*[[".buttons[\"Add Tribe\"].staticTexts[\"Add Tribe\"]",".staticTexts[\"Add Tribe\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let chatViewController = app.otherElements["DiscoverTribesWebViewController"]
+        XCTAssertTrue(chatViewController.exists)
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.

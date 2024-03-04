94b21083d2e9ebf13dd73d63e95b06c001e8c896
Korede612 <korede.oko-osi@nytengaletech.com>
2024-03-04 01:12:57 +0100

    feat: Combine all test function into One to enable CMD + U

--- sphinxUITests/DashboardUITest.swift
+++ sphinxUITests/DashboardUITest.swift
@@ -21,65 +21,56 @@ final class DashboardUITest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testTappingFeedsTabShowFeedContent() throws {
-        let feedTab = app/*@START_MENU_TOKEN@*/.staticTexts["Feed"]/*[[".buttons[\"Feed\"].staticTexts[\"Feed\"]",".staticTexts[\"Feed\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+    func testDashboardUI() {
+        let friendSearchcontent = "Saif"
+        //MARK: - test Tapping Feeds Tab Shows FeedContent
+        let feedTab = app.staticTexts["Feed"]
         XCTAssertTrue(feedTab.exists)
         feedTab.tap()
         let dashboardFeedsContainerVC = app.otherElements["DashboardFeedsContainerViewController"]
         XCTAssertTrue(dashboardFeedsContainerVC.exists)
-    }
-    
-    func testTappingFriendsTabShowFriendsChat() {
+        
+        //MARK: - test Tapping Friends Tab Shows Friends Chat
         let friendsStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Friends"]/*[[".buttons[\"Friends\"].staticTexts[\"Friends\"]",".staticTexts[\"Friends\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         friendsStaticText.tap()
         let chatsContainerVC = app.otherElements["ChatsContainerViewController"]
         XCTAssertTrue(chatsContainerVC.exists)
-    }
-    
-    func testTappingTribesTabShowTribesChats() {
+        
+        //MARK: - test Tapping Tribes Tab Shows Tribes Chats
         app/*@START_MENU_TOKEN@*/.staticTexts["Tribes"]/*[[".buttons[\"Tribes\"].staticTexts[\"Tribes\"]",".staticTexts[\"Tribes\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        let chatsContainerVC = app.otherElements["ChatsContainerViewController"]
         XCTAssertTrue(chatsContainerVC.exists)
-    }
-    
-    func testTappingAddTribesShowDiscoverTribesView() {
+        
+        //MARK: - test Tapping Add Tribes Shows Discover Tribes View
         app/*@START_MENU_TOKEN@*/.staticTexts["Tribes"]/*[[".buttons[\"Tribes\"].staticTexts[\"Tribes\"]",".staticTexts[\"Tribes\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["Add Tribe"]/*[[".buttons[\"Add Tribe\"].staticTexts[\"Add Tribe\"]",".staticTexts[\"Add Tribe\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         let discoverTribesWebVC = app.otherElements["DiscoverTribesWebViewController"]
         XCTAssertTrue(discoverTribesWebVC.exists)
-    }
-    
-    func testTappingLeftArrowButtonBottomBarShowRequestAmountView() {
+        app.buttons["back"].tap()
+        
+        //MARK: - test Tapping Left Arrow Button on Bottom Bar Shows Request Amount View
         app.buttons["bottomBar1"].tap()
         let createInvoiceVC = app.otherElements["CreateInvoiceViewController"]
         XCTAssertTrue(createInvoiceVC.exists)
         app/*@START_MENU_TOKEN@*/.buttons[""]/*[[".otherElements[\"CreateInvoiceViewController\"].buttons[\"\"]",".buttons[\"\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-    }
-    
-    func testTappingRightArrowButtonBottomBarShowRequestAmountView() {
+        
+        //MARK: - test Tapping Right Arrow Button on Bottom Bar Shows Request Amount View
         app.buttons["bottomBar4"].tap()
         let newQRScannerVC = app.otherElements["NewQRScannerViewController"]
         XCTAssertTrue(newQRScannerVC.exists)
         newQRScannerVC.children(matching: .other).element(boundBy: 0)/*@START_MENU_TOKEN@*/.staticTexts[""]/*[[".buttons[\"\"].staticTexts[\"\"]",".staticTexts[\"\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-    }
-    
-    func testTappingScannerButtonBottomBarShowRequestAmountView() {
+        
+        //MARK: - test Tapping Scanner Button on Bottom Bar Shows Request Amount View
         app.buttons["bottomBar3"].tap()
-        let newQRScannerVC = app.otherElements["NewQRScannerViewController"]
         XCTAssertTrue(newQRScannerVC.exists)
         newQRScannerVC.children(matching: .other).element(boundBy: 0)/*@START_MENU_TOKEN@*/.staticTexts[""]/*[[".buttons[\"\"].staticTexts[\"\"]",".staticTexts[\"\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-    }
-    
-    func testTappingTransactionsBottomBarShowTransactionView() {
+        
+        //MARK: - test Tapping Transactions on Bottom Bar Shows Transaction View
         app.buttons["bottomBar2"].tap()
         let historyVC = app.otherElements["HistoryViewController"]
         XCTAssertTrue(historyVC.exists)
         historyVC.children(matching: .other).element.children(matching: .button).element.tap()
-    }
-
-    func testSearchFieldOnFriendsTabFilterChatRow() {
-        let friendSearchcontent = "Saif"
-        let friendsStaticText = app/*@START_MENU_TOKEN@*/.staticTexts["Friends"]/*[[".buttons[\"Friends\"].staticTexts[\"Friends\"]",".staticTexts[\"Friends\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        
+        //MARK: - Search Field on Friends Tab Filter Chat Row based on Search Item
         let searchTextField = app.textFields["Search"]
         friendsStaticText.tap()
         searchTextField.tap()
@@ -90,6 +81,7 @@ final class DashboardUITest: XCTestCase {
         sleep(1)
         app.terminate()
     }
+    
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.

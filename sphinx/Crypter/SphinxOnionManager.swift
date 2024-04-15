254875848744bde41dd35d0ab08232d47fc31f20
Jim Carucci <96802642+uncleJim21@users.noreply.github.com>
2024-04-15 11:11:50 -0500

    fix lock up on restore

--- sphinx/Crypter/SphinxOnionManager.swift
+++ sphinx/Crypter/SphinxOnionManager.swift
@@ -157,7 +157,6 @@ class SphinxOnionManager : NSObject {
     }
 
     func connectToV2Server(contactRestoreCallback: @escaping RestoreProgressCallback, messageRestoreCallback: @escaping RestoreProgressCallback,hideRestoreViewCallback: @escaping ()->()){
-        contactRestoreCallback(0)
         let som = self
         guard let seed = som.getAccountSeed(),
               let myPubkey = som.getAccountOnlyKeysendPubkey(seed: seed),
